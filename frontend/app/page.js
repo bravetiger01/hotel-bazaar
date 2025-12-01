63e8cef6bcb66844b9544210b17b00df76ceece3
Nakul Desai <nakuldesai2006@gmail.com>
2025-12-01 18:36:30 +0530

    hero test

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,15 +1,6 @@
 "use client";
-
 import Hero from '@/components/Hero';
-import ProductCategories from '@/components/ProductCategories';
-import ClearanceSale from '@/components/ClearanceSale';
 
 export default function HomePage() {
-  return (
-    <div>
-      <Hero />
-      <ProductCategories />
-      <ClearanceSale />
-    </div>
-  );
+  return <Hero />;
 }
